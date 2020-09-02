692f9f59b472e2d01e881743c0c452521b1bd083
carpiee <62463826+carpiee@users.noreply.github.com>
2020-09-02 17:03:06 +0200

    added some pdo changes

--- lib/Ticket.php
+++ lib/Ticket.php
@@ -10,31 +10,31 @@ class Ticket{
 
     public function create_ticket($first_name, $last_name, $email, $date, $code, $sesId){
         $this->db->query("INSERT INTO tickets (first_name, last_name, email, event_date, code, session_id) values (:first_name, :last_name, :email, :event_date, :code, :session_id)");
-        $this->db->bind(":first_name", $first_name);
-        $this->db->bind(":last_name", $last_name);
-        $this->db->bind(":email", $email);
-        $this->db->bind(":code", $code);
-        $this->db->bind(":event_date", $date);
-        $this->db->bind(":session_id", $sesId);
+        $this->db->bind(":first_name", $first_name,PDO::PARAM_STR);
+        $this->db->bind(":last_name", $last_name, PDO::PARAM_STR);
+        $this->db->bind(":email", $email, PDO::PARAM_STR);
+        $this->db->bind(":code", $code, PDO::PARAM_STR);
+        $this->db->bind(":event_date", $date, PDO::PARAM_STR);
+        $this->db->bind(":session_id", $sesId, PDO::PARAM_STR);
         return $this->db->execute();
     }
     
     public function check_if_email_has_been_sended($email, $code){
         $this->db->query("SELECT * FROM tickets where email = :email and code = :code LIMIT 1");
-        $this->db->bind(":email", $email);
-        $this->db->bind(":code", $code);
+        $this->db->bind(":email", $email, PDO::PARAM_STR);
+        $this->db->bind(":code", $code, PDO::PARAM_STR);
         return $this->db->single();
     }
 
     public function get_tickets_by_customer($sesId){
         $this->db->query("SELECT * FROM tickets WHERE `session_id` = :sessId");
-        $this->db->bind(':sessId', $sesId);
+        $this->db->bind(':sessId', $sesId, PDO::PARAM_STR);
         return $this->db->resultSet();
     }
     
     public function update_ticket_paid($sesId){
         $this->db->query("UPDATE tickets set betaald = 'true' WHERE `session_id` = :sessId");
-        $this->db->bind(':sessId', $sesId);
+        $this->db->bind(':sessId', $sesId, PDO::PARAM_STR);
         return $this->db->execute();
     }
 }
--- procces.php
+++ procces.php
@@ -23,10 +23,8 @@ if(isset($_POST['first_name'],$_POST['last_name'], $_POST['email'],$_POST['perso
 
 <body>
     <script src="https://js.stripe.com/v3/"></script>
+    <script src="./js/config.js"></script>
     <script>
-    var stripe = Stripe(
-        "pk_test_51Gs6KjG5u9h18At4KiiKkD4DWRya6yGXhymeDyOwdF728FuDcqbRtJkNHfJG2GLiJlPyvMGZe8p9fe2Ho2obdaIH00POOU5VKD"
-    );
     var elements = stripe.elements();
     var ssId = "<?php echo $session_id; ?>";
     const checkout_Id = ssId;
